d833bb059d2a731556a64f62c110e3225ef2e975
Amy Westcott <amy.westcott@yahoo.com>
2020-06-16 21:15:39 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,14 @@ const NavBar = () => {
   debugger
   return (
     <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
+      <NavLink
+        style={{ marginRight: '10px' }}
         to="/"
       >
         Home
       </NavLink>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
+      <NavLink
+        style={{ marginRight: '10px' }}
         to="/movies"
       >
         Movies
